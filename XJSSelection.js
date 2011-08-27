635bc3e8fd04e8ba5539f4e6b47a83386b8a14b6
MONTILLET Xavier <xavierm02.net@gmail.com>
2011-08-27 19:25:33 +0200

    Added XJSSelection.js as usecase.

---
+++ XJSSelection.js
@@ -0,0 +1,85 @@
+// This code is not meant to be used
+// It is just here as a usecase
+// Something similar will be developped in the future
+// But at the moment, you shouldn't rely on it
+
+( function ( ) {
+    var undefined;
+    var defaultWindow = this;
+    
+    function toArray( array ) {
+        return [ ].slice.call( array );
+    }
+    
+    function XJSSelection( window ) {
+        if ( arguments.length < 1 ) {
+            window = defaultWindow;
+        }
+        var selection;
+        if ( typeof window.getSelection === "function" ) {
+            selection = window.getSelection( );
+        } else if ( window.document.selection ) {
+            selection = window.document.selection;
+        } else {
+            throw new Error( 'Your JavaScript implementation does not support "window.getSelection".' );
+        }
+        var accessors = new XJSAccessors( this );
+        accessors.defineProperties( [
+            {
+                name: 'selection',
+                value: selection,
+                isProtected: true
+            },
+            {
+                name: 'range',
+                isProtected: true,
+                getter: function ( name, index ) {
+                    if ( arguments.length < 2 ) {
+                        index = 0;
+                    } else {
+                        index = +index;
+                    }
+                    if ( index < 0 || index >= this.get( 'rangeCount' ) ) {
+                        throw new Error( 'There is no range at index ' + index + '!' );
+                    }
+                    var range;
+                    if ( typeof selection.getRangeAt === 'function' ) {
+                        range = selection.getRangeAt( index );
+                    } else if ( typeof selection.createRangeCollection === 'function' ) {
+                        range = selection.createRangeCollection( )[ index ];
+                    } else {
+                        throw new Error( 'Your JavaScript implementation does not support "Selection.prototype.getRangeAt".' );
+                    }
+                    return new XJSRange( window, range );
+                }
+            },
+            {
+                name: 'rangeCount',
+                isProtected: true,
+                getter: function ( ) {
+                    return selection.rangeCount;
+                }
+            }
+        ] );
+        if ( ! selection.hasOwnProperty( 'rangeCount' ) ) {
+            accessors.setGetter( 'rangeCount', function ( ) {
+                if ( typeof selection.createRangeCollection === 'function' ) {
+                    return selection.createRangeCollection( ).length;
+                } else {
+                    throw new Error( 'Your JavaScript implementation does not support "Selection.rangeCount".' );
+                }
+            } );
+        }
+    }
+    
+    function XJSRange( window, range ) {
+        new XJSAccessors( this ).defineProperties( [
+            {
+                name: 'range',
+                value: range
+            }
+        ] );
+    }
+    
+    this.XJSSelection = XJSSelection;
+} )( );
